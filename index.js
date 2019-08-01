7bd1b33e4f59c9344003d4d8387a6648c41067e7
ahmetkorkmaz3 <ahmetkorkmaz3453@gmail.com>
2019-08-01 13:05:06 +0300

    nodejs stream oluşturuldu

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+
+var app = express();
+
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+var Log = require('log'),
+    log = new Log('debug')
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.get('/', function(req, res){
+  res.redirect('index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('stream', function(image){
+    socket.broadcast.emit('stream', image);
+  });
+});
+
+http.listen(port, function(){
+  console.log("server run %s", port);
+});
